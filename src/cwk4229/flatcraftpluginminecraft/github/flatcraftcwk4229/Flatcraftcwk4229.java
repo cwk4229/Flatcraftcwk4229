6176f1bd847976e06f7817125512346494fce732
Owen <computewizkid4229@outlook.com>
2013-05-06 19:21:57 +0100

    Main files

---
+++ src/cwk4229/flatcraftpluginminecraft/github/flatcraftcwk4229/Flatcraftcwk4229.java
@@ -0,0 +1,18 @@
+package cwk4229.flatcraftpluginminecraft.github.Flatcraftcwk4229.class
+
+import java.util.logging.Logger;
+
+ public class Flatcraftcwk4229 extends JAvaPlugin{
+	 public final Logger logger = Logger.getLogger("Minecraft");
+	 public static Flatcraftcwk4229 plugin;
+	 
+	 @Override
+	 public void onDisable(){
+		 
+	 }
+	 
+	 @override
+	 public void onEnable(){
+		 
+	 }
+ }
